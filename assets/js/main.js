6d9d3492c344b893676fda82cc363fc51aa3446e
KingCreatBug <trunghoang.240500@gmail.com>
2024-08-29 17:13:41 +0700

    add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,79 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+//Carousel
+const carousel = document.querySelector(".create-container__carousel"),
+    firstItem = document.querySelectorAll(".create-container__box")[0],
+    arrowIcons = document.querySelectorAll(".create-content__btn"),
+    unMarginLeft = document.querySelector(".create-container");
 
-/**
- * Hàm tải template
- *
- * Cách dùng:
- * <div id="parent"></div>
- * <script>
- *  load("#parent", "./path-to-template.html");
- * </script>
- */
-function load(selector, path) {
-    const cached = localStorage.getItem(path);
-    if (cached) {
-        $(selector).innerHTML = cached;
+let isDragStart = false,
+    prevPageX,
+    prevScrollLeft,
+    positionDiff;
+let firstItemWidth = firstItem.clientWidth + 14;
+
+arrowIcons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+        carousel.scrollLeft +=
+            icon.id == "left" ? -firstItemWidth : firstItemWidth;
+    });
+});
+
+const autoSlide = () => {
+    positionDiff = Math.abs(positionDiff);
+    let firstItemWidth = firstItem.clientWidth + 14;
+    let valDifference = firstItemWidth - positionDiff;
+
+    if (carousel.scrollLeft > prevScrollLeft) {
+        return (carousel.scrollLeft +=
+            positionDiff > firstItemWidth / 3 ? valDifference : -positionDiff);
     }
+    carousel.scrollLeft -=
+        positionDiff > firstItemWidth / 3 ? valDifference : -positionDiff;
+};
 
-    fetch(path)
-        .then((res) => res.text())
-        .then((html) => {
-            if (html !== cached) {
-                $(selector).innerHTML = html;
-                localStorage.setItem(path, html);
-            }
-        })
-        .finally(() => {
-            window.dispatchEvent(new Event("template-loaded"));
-        });
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+    if (!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    // unMarginLeft.style.marginLeft = "20px";
+};
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+    autoSlide();
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
+
+// Slide text
+function slideText() {
+    const e = document.querySelectorAll();
+    let width = 0;
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (width == 100) {
+            width = 0;
+        } else {
+            width++;
+            element.style.width = width + "%";
+        }
+    }
 }
+
+slideText();
